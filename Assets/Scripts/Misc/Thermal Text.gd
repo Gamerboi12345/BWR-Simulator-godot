f9d230c21b5068e8d7e4c1d0785e00b7222a1b5a
Gamerboi12345 <95109681+Gamerboi12345@users.noreply.github.com>
2023-08-15 13:18:49 -0700

    Changed Thermal Power Display to Percent

--- Assets/Scripts/Misc/Thermal Text.gd
+++ Assets/Scripts/Misc/Thermal Text.gd
@@ -5,6 +5,6 @@ extends Label3D
 
 func _ready():
 	while true:
-		var power = $"/root/Node3d/Reactor".total_core_power_percent*3600
+		var power = $"/root/Node3d/Reactor".total_core_power_percent
 		therm_text.text = str(power)
 		await get_tree().create_timer(0.1).timeout
